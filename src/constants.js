40408c5451aa9f16de5224d413a2c6ad2fba02d6
steambot33 <steambot33@gmail.com>
2020-11-02 00:34:38 +0300

    [fix] LINK constant

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const LINK = "http://localhost:3000";
+export const LINK = "http://google.com/";
 export const TIMEOUT = 1000;
 export const SWAL_TITLE = "Thank you!";
 export const SWAL_MESSAGE = "You've registered successfully.";
